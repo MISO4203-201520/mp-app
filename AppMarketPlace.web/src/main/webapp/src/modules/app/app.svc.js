5355c954e0120cac6ae6483b5a2e43ab3d91f6ea
Jhonatan Alarcón <jj.alarcon10@uniandes.edu.co>
2015-08-25 12:29:45 -0500

    Se elimina pj para correccion
    Se elimina pj para correccion

--- AppMarketPlace.web/src/main/webapp/src/modules/app/app.svc.js
+++
@@ -1,7 +0,0 @@
-(function(ng){
-    var mod = ng.module('appModule');
-    
-    mod.service('appService', ['CrudCreator','appContext', function(CrudCreator, context){
-            CrudCreator.extendService(this, context);
-    }]);
-})(window.angular);
