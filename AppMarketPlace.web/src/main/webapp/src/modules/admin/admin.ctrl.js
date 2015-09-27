bcd81e7d9af8880f7b84cb0d8a3823aa17410b02
César Forero <cesar.forero.gz@gmail.com>
2015-09-26 22:25:10 -0500

    Add link and security to access to admin page.

--- AppMarketPlace.web/src/main/webapp/src/modules/admin/admin.ctrl.js
+++ AppMarketPlace.web/src/main/webapp/src/modules/admin/admin.ctrl.js
@@ -4,29 +4,35 @@
 
     mod.controller('adminClientCtrl', ['CrudCreator', '$scope', 'clientService', 'clientModel', 'authService', 'adminService','$location', function (CrudCreator, $scope, svc, model, authSvc, adminSvc, $location) {
         CrudCreator.extendController(this, svc, $scope, model, 'admin', 'Clients');
-        showList(authSvc,adminSvc,$location,$scope,this,true);        
+        showList($scope,this,true);        
     }]);
     mod.controller('adminDevCtrl', ['CrudCreator', '$scope', 'developerService', 'developerModel', 'authService', 'adminService','$location', function (CrudCreator, $scope, svc, model, authSvc, adminSvc, $location) {
         CrudCreator.extendController(this, svc, $scope, model, 'admin', 'Developers');
-        showList(authSvc,adminSvc,$location,$scope,this,false); 
+        showList($scope,this,false); 
     }]);
     
-    var showList =function(authSvc,adminSvc,$location,$scope,self,first){
-        if(authSvc.getCurrentUser()){
-            adminSvc.fullUser().then(function(data){ 
-                if(data.role==='admin'){
-                    $scope.listUsers = {
-                        clients:first,
-                        devs:!first
-                    }; 
-                    self.fetchRecords();       
-                }else{
-                    $location.path('/catalog');
-                }
-            });           
-        }else{
-            $location.path('/catalog');
-        }
+    //var showList =function(authSvc,adminSvc,$location,$scope,self,first){
+     var showList =function($scope,self,first){
+        $scope.listUsers = {
+            clients:first,
+            devs:!first
+        }; 
+        self.fetchRecords();    
+//        if(authSvc.getCurrentUser()){
+//            adminSvc.fullUser().then(function(data){ 
+//                if(data.role==='admin'){
+//                    $scope.listUsers = {
+//                        clients:first,
+//                        devs:!first
+//                    }; 
+//                    self.fetchRecords();       
+//                }else{
+//                    $location.path('/catalog');
+//                }
+//            });           
+//        }else{
+//            $location.path('/catalog');
+//        }
     };
    
 })(window.angular);
