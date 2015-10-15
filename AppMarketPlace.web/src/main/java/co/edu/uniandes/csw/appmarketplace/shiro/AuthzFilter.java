c2f4489093b12bd6c4dc0143af3bf1c4139b5d79
d.jimenez13 <d.jimenez13@uniandes.edu.co>
2015-10-14 20:08:20 -0500

    Autenticación shiro por url y métodos

--- AppMarketPlace.web/src/main/java/co/edu/uniandes/csw/appmarketplace/shiro/AuthzFilter.java
+++ AppMarketPlace.web/src/main/java/co/edu/uniandes/csw/appmarketplace/shiro/AuthzFilter.java
@@ -8,6 +8,7 @@ package co.edu.uniandes.csw.appmarketplace.shiro;
 import java.io.IOException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.web.filter.authz.RolesAuthorizationFilter;
@@ -22,6 +23,30 @@ public class AuthzFilter extends RolesAuthorizationFilter {
     protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws IOException {
         ((HttpServletResponse)response).sendError(HttpServletResponse.SC_FORBIDDEN);   
         return SecurityUtils.getSubject().isAuthenticated();
+    }
+    
+    @Override
+    public boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws IOException {
+        if (mappedValue != null) {
+            String[] methodRef = (String[]) mappedValue;
+            for (String methodRef1 : methodRef) {
+                String[] data = methodRef1.split(":");
+                if (data.length > 1) {
+                    String[] methods = data[1].split("-");
+                    for (String method : methods) {
+                        if (((HttpServletRequest) request).getMethod().equals(method)) {
+                            return SecurityUtils.getSubject().hasRole(data[0]);
+                        }
+                    }
+                } else {
+                    return super.isAccessAllowed(request, response, mappedValue);
+                }
+                return true;
+            }
+            return true;
+        } else {
+            return true;
+        }
     }
 
 }
